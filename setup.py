9d571a3b49a13d91760b223dbb24080a5a0798ff
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-12-29 18:58:52 +0100

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='LeanSim',
+    version='0.1',
+    packages=['leansim'],
+    url='',
+    license='',
+    author='Nicholas A. Del Grosso',
+    author_email='delgrosso.nick@gmail.com',
+    description='A simple Lean production simulation, meant for exploring lean management concepts and sharing in a lecture setting.'
+)
